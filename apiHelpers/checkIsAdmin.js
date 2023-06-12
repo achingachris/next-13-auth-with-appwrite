5368a33efd37dd22f72befb9eda483a279fbd6f5
chrisOnWSL <achinga.chris@gmail.com>
2023-06-12 18:09:53 +0300

    api helpers

---
+++ apiHelpers/checkIsAdmin.js
@@ -0,0 +1,21 @@
+import sdk from "node-appwrite"
+import getJWTClient from "./getJWTClient"
+
+const ADMIN_ROLE_ID = process.env.ADMIN_TEAM_ID
+
+async function checkIsAdmin(req) {
+  const jwtClient = getJWTClient(req)
+
+  // Check if user belongs to admin team
+  const teams = new sdk.Teams(jwtClient)
+
+  try {
+    await teams.get(ADMIN_ROLE_ID)
+    return true
+  } catch (exception) {
+    // User is not an admin
+    return false
+  }
+}
+
+export default checkIsAdmin
---
+++ apiHelpers/getJWTClient.js
@@ -0,0 +1,21 @@
+import { environment } from "../utils"
+import sdk from "node-appwrite"
+
+// Client that verifies whether user has access to the Users collection
+const jwtClient = new sdk.Client()
+
+jwtClient
+  .setEndpoint(`${environment.hostname}/v1`)
+  .setProject(environment.projectId)
+
+function getJWTClient(req) {
+  const { headers } = req
+  const { authorization } = headers
+
+  // Get JWT from request
+  const jwt = authorization?.replace("Bearer ", "") || ""
+  jwtClient.setJWT(jwt)
+  return jwtClient
+}
+
+export default getJWTClient
